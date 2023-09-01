7a2a913ef07a3c4d19ed6ef7aaf59dcdc62e77ad
Helen85236 <79532870251@yandex.ru>
2023-09-02 00:19:44 +0300

    controller for clients

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from newsletters.forms import ClientForm
 from newsletters.models import Client
@@ -17,4 +17,29 @@ class ClientListView(ListView):
 class ClientCreateView(CreateView):
     model = Client
     form_class = ClientForm
+    success_url = reverse_lazy('newsletters:client_list')
+
+    # Basic validation using models fields
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+
+    def get_success_url(self):
+        return reverse('newsletters:client_update', args=[self.object.pk])
+
+    # Basic validation using models fields
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
     success_url = reverse_lazy('newsletters:client_list')
