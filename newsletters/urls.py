4173fc406b50ddd1a2d8c9037beffcb971df6b2e
Helen85236 <79532870251@yandex.ru>
2023-09-01 23:52:11 +0300

    Work with newsletters

--- newsletters/urls.py
+++ newsletters/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from newsletters.apps import NewslettersConfig
-from newsletters.views import index
+from newsletters.views import index, ClientListView, ClientCreateView
 
 app_name = NewslettersConfig.name
 
 urlpatterns = [
-   path('', index, name='newsletters')
+   path('', ClientListView.as_view(), name='client_list'),
+   path('client/create', ClientCreateView.as_view(), name='client_create'),
 ]
--- newsletters/views.py
+++ newsletters/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+
+from newsletters.forms import ClientForm
+from newsletters.models import Client
 
 def index(request):
     # Testing
     return render(request, 'newsletters/base.html')
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('newsletters:client_list')
